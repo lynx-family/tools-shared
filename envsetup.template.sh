e63bdc8475c9ee7d1047c67c7a5e5b6451cde8db
scanf3 <981544717@qq.com>
2025-03-05 20:12:05 +0800

    [Infra] update the readme file and update template file
    As above

--- envsetup.template.sh
+++ envsetup.template.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
-# Copyright 2024 The Lynx Authors. All rights reserved.
+# Copyright 2025 The Lynx Authors. All rights reserved.
 # Licensed under the Apache License Version 2.0 that can be found in the
 # LICENSE file in the root directory of this source tree.
-# This is a template for the envsetup.sh file, used to download a specific version of lcm_tools
-# in a specific repository. Any repository that needs to specify a specific lcm_tools version
+
+# This is a template for the envsetup.sh file, used to download a specific version of tools_shared
+# in a specific repository. Any repository that needs to specify a specific tools_shared version
 # can copy this template to the repository, and then prompt developers to execute this script
-# using the source command before using lcm_tools.
+# using the source command before using tools_shared.
 
 # Modify the variable below to select a specific version
-LCM_TOOLS_REVISION=latest
+TOOLS_SHARED_REVISION=latest
 
-LCM_TOOLS_REPO_URL=
+TOOLS_SHARED_REPO_URL=git@github.com:lynx-family/tools-shared.git
 
-# set default LCM_TOOLS_HOME 
-if [[ -z "$LCM_TOOLS_HOME" ]]; then
-    LCM_TOOLS_HOME=$HOME/.lcm_tools
+# set default TOOLS_SHARED_HOME 
+if [[ -z "$TOOLS_SHARED_HOME" ]]; then
+    TOOLS_SHARED_HOME=$HOME/.tools_shared
 fi
 
-if [ ! -d "$LCM_TOOLS_HOME" ]; then
-  git clone $LCM_TOOLS_REPO_URL "$LCM_TOOLS_HOME"
-elif [ ! -d "$LCM_TOOLS_HOME/.git" ]; then
-  echo "Error: LCM Tools directory $LCM_TOOLS_HOME not empty, Please delete it first."
+if [ ! -d "$TOOLS_SHARED_HOME" ]; then
+  git clone $TOOLS_SHARED_REPO_URL "$TOOLS_SHARED_HOME"
+elif [ ! -d "$TOOLS_SHARED_HOME/.git" ]; then
+  echo "Error: Tools Shared directory $TOOLS_SHARED_HOME not empty, Please delete it first."
   exit 1
 else
-  pushd "$LCM_TOOLS_HOME" > /dev/null || exit
+  pushd "$TOOLS_SHARED_HOME" > /dev/null || exit
   if ! git status -uno > /dev/null; then
     git reset --hard
     git clean -fdx
   fi
   
-  if [[ $LCM_TOOLS_REVISION = "latest" ]]; then
+  if [[ $TOOLS_SHARED_REVISION = "latest" ]]; then
     git checkout --quiet --force master > /dev/null && git pull > /dev/null
   else
-    git fetch --force --progress --update-head-ok -- $LCM_TOOLS_REPO_URL $LCM_TOOLS_REVISION
+    git fetch --force --progress --update-head-ok -- $TOOLS_SHARED_REPO_URL $TOOLS_SHARED_REVISION
     git checkout FETCH_HEAD --quiet
   fi
   popd > /dev/null || exit
 fi
 
-export LCM_AUTO_UPDATE=0
-source "$LCM_TOOLS_HOME"/envsetup.sh "$@"
+source "$TOOLS_SHARED_HOME"/envsetup.sh "$@"
